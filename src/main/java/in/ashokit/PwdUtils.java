354fd6ead1c1ac9416cfdbf6b2c9e838b475733a
Sanjeev <kumarsanjugt@gmail.com>
2022-01-28 00:47:01 -0800

    Bug Fix:6778

--- src/main/java/in/ashokit/PwdUtils.java
+++ src/main/java/in/ashokit/PwdUtils.java
@@ -3,10 +3,10 @@ package in.ashokit;
 public class PwdUtils {
 
 	public void encode() {
-		
+		System.out.println("encode!");
 	}
 
 	public void decode() {
-		
+		System.out.println("decode!");
 	}
 }
